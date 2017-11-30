a6cebcecd0fc71ee0a9439bfe2d5e3b523fa15bc
Oscar A. Argueta H <oscar.argueta@atel.com.gt>
2017-11-29 18:03:12 -0600

    Inicio modulo Ventas
    * Se modifico el layout, para agregar un GridPane
    * Se agrego el textbox para la fecha y se agrego un Listener para poder validar y formatear la fecha.

--- src/main/java/com/servicont/ingresoventas/FXMLController.java
+++ src/main/java/com/servicont/ingresoventas/FXMLController.java
@@ -2,16 +2,21 @@ package com.servicont.ingresoventas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class FXMLController implements Initializable {
     
     @FXML
     private Label label;
-    
+    @FXML
+    private TextField tfFecha;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -21,5 +26,19 @@ public class FXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        try{
+            tfFecha.textProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, 
+                String newValue) {
+                    if (!newValue.matches("\\d*")) {
+                        tfFecha.setText(newValue.replaceAll("[^\\d]", ""));
+                    }
+                }
+            });
+        }catch(Exception ex){
+            ex.printStackTrace();
+            System.err.println("ERROR : ["+ex.getMessage()+"]");
+        }
+    }
 }
